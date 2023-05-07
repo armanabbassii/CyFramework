0fb3e7dcc0d3167c962622978fd72ead6174e71d
Arman Abbasi <arman.abbasi.nt@gmail.com>
2023-05-07 21:47:50 +0430

    tst

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import 'cypress-mochawesome-reporter/register';
+import "cypress-real-events";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
